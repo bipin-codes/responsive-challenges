c77300857b1f75c4e597a5871e78af2ec18aa612
bipin <bipincodes@outlook.com>
2022-11-25 21:16:24 +0530

    Added ingredients and instructions of the recipe page

--- src/routes/Recipe/styles.ts
+++ src/routes/Recipe/styles.ts
@@ -2,11 +2,11 @@ import {
   BlankContainer,
   Container,
   Heading,
-} from "components/common-styled/common";
-import styled from "styled-components";
+} from 'components/common-styled/common';
+import styled from 'styled-components';
 
 export const StyledContainer = styled(Container)`
-  font-family: "Playfair Display", serif;
+  font-family: 'Playfair Display', serif;
   padding: 2rem 15rem;
 `;
 export const StyledHeading = styled(Heading)`
@@ -21,7 +21,7 @@ export const StyledDescription = styled(BlankContainer)`
   align-items: flex-start;
   p {
     font-weight: 500;
-    font-family: "Montserrat", sans-serif;
+    font-family: 'Montserrat', sans-serif;
     font-style: italic;
     margin-left: 0.5rem;
   }
@@ -39,7 +39,47 @@ export const RecipeTitle = styled(Heading)`
   font-weight: 400;
   font-style: italic;
   font-size: 1.125rem;
-  margin: 1rem 0;
+  margin: 2rem 0;
 `;
-export const RecipeItems = styled.ul``;
-export const RecipeItem = styled.li``;
+export const RecipeItems = styled.ul`
+  list-style-type: none;
+`;
+export const RecipeItem = styled.li`
+  display: flex;
+  font-family: 'Montserrat', sans-serif;
+  margin: 1rem 0;
+
+  div {
+    border-radius: 4px;
+    margin-right: 1rem;
+    height: 21px;
+    width: 21px;
+    border: 1px solid #828282;
+  }
+  p {
+    font-size: 1rem;
+    flex: 1;
+  }
+`;
+
+export const Instruction = styled(BlankContainer)`
+  margin: 2rem 0;
+  display: flex;
+`;
+export const Checkbox = styled(BlankContainer)``;
+export const Idx = styled.span`
+  background: #f2994a;
+  padding: 0.25rem 0.5rem;
+  border-radius: 6px;
+  font-family: 'Playfair Display', serif;
+  margin-right: 1rem;
+  color: white;
+  font-size: 1.5rem;
+  align-self: flex-start;
+`;
+export const InstructionDescription = styled.span``;
+export const InstructionHeading = styled.span`
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  font-size: 1rem;
+`;
