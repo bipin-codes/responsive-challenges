e605df2701163541f0c4d89eab836b1bda3426ec
bipingosain <bipingosain3492@gmail.com>
2022-11-27 16:43:27 +0530

    styled the index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 const Links = [
-  { to: '/scarecrow', label: 'Not found Challenge.' },
-  { to: '/teams', label: 'My Team Challenge.' },
-  { to: '/interior', label: 'Interior Consultant Challenge.' },
-  { to: '/recipe', label: 'Recipe page Challenge.' },
-  { to: '/gallery', label: 'Gallery Challenge.' },
+  { to: "/scarecrow", label: "Not found Challenge." },
+  { to: "/teams", label: "My Team Challenge." },
+  { to: "/interior", label: "Interior Consultant Challenge." },
+  { to: "/recipe", label: "Recipe page Challenge." },
+  { to: "/gallery", label: "Gallery Challenge." },
 ];
 const App = () => {
   return (
-    <ul>
-      {Links.map((route) => (
-        <li>
-          <Link to={route.to!}>{route.label}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2 className="title">Responsive Challenges</h2>
+      <h6 className="sub-title">
+        Please select any one of the below links to view the challenge.
+      </h6>
+      <ul className="routes-list">
+        {Links.map((route) => (
+          <li className="route-item">
+            <Link to={route.to!}>{route.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
