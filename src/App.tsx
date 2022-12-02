8c1aaca9043a30ed2d1721fb85f514cceb63a8bd
bipingosain <bipingosain3492@gmail.com>
2022-12-02 14:59:53 +0530

    fixed the layout on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
 const Links = [
-  { to: '/scarecrow', label: 'Not found challenge.' },
-  { to: '/teams', label: 'My team challenge.' },
-  { to: '/interior', label: 'Interior consultant challenge.' },
-  { to: '/recipe', label: 'Recipe page challenge.' },
-  { to: '/gallery', label: 'Gallery challenge.' },
-  { to: '/checkout', label: 'Checkout page challenge.' },
-  { to: '/homepage', label: 'Eddie home challenge.' },
+  { to: "/scarecrow", label: "Not found challenge." },
+  { to: "/teams", label: "My team challenge." },
+  { to: "/interior", label: "Interior consultant challenge." },
+  { to: "/recipe", label: "Recipe page challenge." },
+  { to: "/gallery", label: "Gallery challenge." },
+  { to: "/checkout", label: "Checkout page challenge." },
+  { to: "/homepage", label: "Eddie home challenge." },
 ];
 const App = () => {
   return (
-    <>
+    <div className="home">
       <h2 className="title">Responsive Challenges</h2>
       <h6 className="sub-title">
         Please select any one of the below links to view the challenge.
@@ -24,7 +24,7 @@ const App = () => {
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
 
